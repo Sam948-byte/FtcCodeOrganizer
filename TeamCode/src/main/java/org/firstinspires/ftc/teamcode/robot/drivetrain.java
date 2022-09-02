c9d4154c270fe2a006bd829349a99f172d2291a8
Sam <techguy763@gmail.com>
2022-09-02 16:29:05 -0500

    Update drivetrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/drivetrain.java
@@ -1,1 +1,17 @@
+package org.firstinspires.ftc.teamcode.robot;
 
+public class drivetrain extends Subsystem{
+    //instance variables here
+    
+    public Example() {
+        super();
+        //constructor code here
+    }
+    
+    @Override
+    public void manualControl() {
+        //teleop code here
+    }
+    
+    //other methods here
+}
